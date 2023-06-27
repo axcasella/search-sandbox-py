098f4a906be33a6fb7607f3e6986efbee7d4da97
axcasella <axc@bu.edu>
2023-06-26 23:56:47 -0400

    added q&a

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+import pickle
+import os
 
 # Sidebar contents
 with st.sidebar:
@@ -14,14 +21,19 @@ with st.sidebar:
 
 # Main page contents
 def main():
+    load_dotenv()
+
     st.header("Search input")
 
     pdf = st.file_uploader("Upload a PDF file", type=["pdf"])
+    
 
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
+        # Get rid of .pdf from name
+        store_file_name = pdf.name[:-4]
+       
         text = ""
-
         for page in pdf_reader.pages:
             text += page.extract_text()
         
@@ -29,7 +41,40 @@ def main():
         splitter = RecursiveCharacterTextSplitter(separators=[".", ",", "\n"], chunk_size=1000, chunk_overlap=100)
         chunks = splitter.split_text(text=text)
 
-        st.write(chunks)
+        if os.path.exists(f"{store_file_name}.pk1"):
+            # read the file
+            with open(f"{store_file_name}.pk1", "rb") as f:
+                vectorStore = pickle.load(f)
+
+            st.write('File already exists, loaded embeddings from disk')
+        else:
+            # Embed chunks
+            embeddings = OpenAIEmbeddings()
+
+            # Create vector store using Meta's FAISS store
+            vectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+
+            with open(f"{store_file_name}.pk1", "wb") as f:
+                pickle.dump(vectorStore, f)
+            
+            st.write('New, created vector store')
+
+        # Take in search query
+        query = st.text_input("Search for:")
+        if query:
+            # Get top results
+            docs = vectorStore.similarity_search(query=query, k=5)
+
+            # Ask LLM to give final result
+            llm = OpenAI(temperature=0)
+            chain = load_qa_chain(llm=llm, chain_type="stuff")
+            response = chain.run(input_documents=docs, question=query)
+            st.write("Answer:")
+            st.write(response)
+
+            st.write("Top results:")
+            st.write(docs)
+
 
 if __name__ == "__main__":
     main()
