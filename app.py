b4bc02dea5982f476a2d5ea2f9e26e7f1710c600
axcasella <axc@bu.edu>
2023-06-30 00:13:37 -0400

    refactor; added chaining and template

--- app.py
+++ app.py
@@ -2,21 +2,25 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
+from langchain import LLMChain
+from langchain.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import (SystemMessagePromptTemplate, HumanMessagePromptTemplate, PromptTemplate, ChatPromptTemplate)
+
 import pickle
 import os
 
 # Sidebar contents
-with st.sidebar:
-    st.title('Demo of LLM semantic search')
-    add_vertical_space(3)
-    st.markdown('''
-    ### Made by Alex Casella
-    ''')
+# with st.sidebar:
+#     st.title('Demo of LLM semantic search')
+#     add_vertical_space(3)
+#     st.markdown('''
+#     ### Made by Alex Casella
+#     ''')
 
 # Main page contents
 def main():
@@ -26,7 +30,28 @@ def main():
 
     pdf = st.file_uploader("Upload a PDF file", type=["pdf"])
     
+    vector_store, store_file_name = create_vector_store_from_pdf(pdf)
+    if vector_store and store_file_name is not None:
+        num_vectors = vector_store.index.ntotal
+        st.write(f"New file, created vector store with {num_vectors} vectors")
 
+        with open(f"{store_file_name}.pk1", "wb") as f:
+            pickle.dump(vector_store, f)
+            
+        # Take in search query
+        query = st.text_input("Search for:")
+        if query:
+            # Get top results
+            k = 3
+            response, docs = get_response_from_query(vector_store, query, k=k)
+            st.write("Answer:")
+            st.write(response)
+
+            st.write(f"Top {k} results:")
+            cleaned_docs = [doc.page_content.replace('\n', ' ') for doc in docs]
+            st.write(cleaned_docs)
+
+def create_vector_store_from_pdf(pdf):
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
         # Get rid of .pdf from name
@@ -46,39 +71,45 @@ def main():
             # Use langchain to split into chunks
             splitter = RecursiveCharacterTextSplitter(["\n\n", "\n", ".", ","], chunk_size=1000, chunk_overlap=100)
             chunks = splitter.split_text(text=text)
-            # st.write("Chunk:")
             st.write("File broken into chunks: ", len(chunks))
-            # st.write(chunks)
+            st.write("Chunk:")
+            st.write(chunks)
 
             # Embed chunks
             embeddings = OpenAIEmbeddings()
 
             # Create vector store using Meta's FAISS store
             vectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            num_vectors = vectorStore.index.ntotal
-            st.write(f"New file, created vector store with {num_vectors} vectors")
 
-            with open(f"{store_file_name}.pk1", "wb") as f:
-                pickle.dump(vectorStore, f)
-            
-        # Take in search query
-        query = st.text_input("Search for:")
-        if query:
-            # Get top results
-            k = 5
-            docs = vectorStore.similarity_search(query=query, k=k)
+        return vectorStore, store_file_name
+    else: 
+        return None, None
 
-            # Ask LLM to give final result
-            llm = OpenAI(temperature=0)
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            response = chain.run(input_documents=docs, question=query)
-            st.write("Answer:")
-            st.write(response)
+def get_response_from_query(db, query, k=3):
+    docs = db.similarity_search(query=query, k=k)
+    docs_page_content = " ".join([doc.page_content for doc in docs])
 
-            st.write(f"Top {k} results:")
-            cleaned_docs = [doc.page_content.replace('\n', ' ') for doc in docs]
-            st.write(cleaned_docs)
+    # Ask LLM to give final result
+    llm = OpenAI(model_name="gpt-3.5-turbo", temperature=0.2)
 
+    system_template = """
+        Only use factual information from the document {docs}.
+
+        If you don't have enough information, just say "I don't know".
+    """
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+
+    human_template = """
+        Anser the question: {question}
+    """
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+
+    chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
+
+    chain = LLMChain(llm=llm, prompt=chat_prompt)
+    response = chain.run(docs=docs_page_content, question=query)
+
+    return response, docs
 
 if __name__ == "__main__":
     main()
