5e3d82617c03bc69da54712193016804f5d2e85b
axcasella <axc@bu.edu>
2023-07-21 18:51:26 -0400

    updated directory

--- llm/huggingfacelocal_demo.py
+++ llm/huggingfacelocal_demo.py
@@ -1,11 +1,10 @@
 from langchain.llms import HuggingFacePipeline
+from transformers import AutoTokenizer, pipeline, AutoModelForSeq2SeqLM
 import torch
-from transformers import AutoTokenizer, AutoModelForCasualLM, pipeline, AutoModelForSeq2SeqLM
 
 model_id = "google/flan-t5-large"
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForSeq2SeqLM.from_pretrained(model_id, load_in_8_bit=True)
-
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
 
 pipe = pipeline("text2text-generation", model=model, tokenizer=tokenizer, max_length=100)
 local_llm = HuggingFacePipeline(pipeline=pipe)
--- ocr/aws_textract_ocr.py
+++ ocr/aws_textract_ocr.py
@@ -10,7 +10,7 @@ zoom_x = 2.0  # horizontal zoom
 zoom_y = 2.0  # vertical zoom
 mat = fitz.Matrix(zoom_x, zoom_y)  # zoom factor 2 in each dimension
 
-path = 'documents/'
+path = '../documents/'
 all_files = glob.glob(path + "*.pdf")
 
 # AWS client
@@ -22,7 +22,7 @@ for filename in all_files:
     doc = fitz.open(filename)  # open document
     for page in doc:  # iterate through the pages
         pix = page.get_pixmap(matrix=mat)  # render page to an image
-        outputname = "data/out/page-%i.png" % page.number
+        outputname = "../data/out/page-%i.png" % page.number
         pix.save(outputname)  # store image as a PNG
 
         with open(outputname, 'rb') as image:
